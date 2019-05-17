9966ae5414e9e986db7c79ec8d1ffd773dd954a8
muxanz <muxanzz@gmail.com>
2019-05-17 02:36:42 +0000

    Add decision_taker.c

---
+++ decision_taker.c
@@ -0,0 +1,61 @@
+#include "monty.h"
+
+#define UNUSED(x) (void)(x)
+
+/**
+ *
+ *
+ *
+ *
+ *
+*/
+
+void decision_taker(char *opcode, char *op_arg, unsigned int line_number, stack_t **stack)
+{
+	int i;
+	void (*selection)(stack_t **, unsigned int) = NULL;
+
+	instruction_t options[] =
+	{
+		{"push", &_push},
+		{"pop", &_pop},
+		{"swap", &_swap},
+		{"add", &_add},
+		{"pall", &_pall},
+		{"pint", &_pint},
+		{"nop", &_nop},
+		{NULL, NULL}
+	};
+
+	UNUSED(selection);
+
+	while (i < 8)
+	{
+		if ((strcmp(opcode, options[i].opcode)) == 0)
+		{
+			if ((strcmp(options[i].opcode, "push")) == 0)
+			{
+				if (op_arg)
+				{
+					value = atoi(op_arg);
+				}
+				else
+				{
+					fprintf(stderr, "L%d: usage: push integer\n", line_number);
+					exit(EXIT_FAILURE);
+				}
+			}
+			selection = options[i].f;
+
+			(*selection)(stack, line_number);
+			return;
+		}
+		i++;
+	}
+	if (i == 8)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s", line_number, opcode);
+		/*LOOK FOR SOMETHING TO FREE*/
+		exit(EXIT_FAILURE);
+	}
+}
