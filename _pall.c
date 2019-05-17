dfca260ccdfdce9c76f1b6f7cfc9b3df7304ff3e
muxanz <muxanzz@gmail.com>
2019-05-17 04:26:00 +0000

    corrects betty

--- _pall.c
+++ _pall.c
@@ -3,12 +3,15 @@
 #define UNUSED(x) (void)(x)
 
 /**
+ * _pall - prints all the values on the stack, starting from the top of the stack.
  *
+ *@stack: pointer to the memory stack
  *
+ *@line_number: actual line number of the current opened file
  *
- *
- *
-*/
+ * Return: void.
+ */
+
 
 void _pall(stack_t **stack, unsigned int line_number)
 {
