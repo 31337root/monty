3a7080af939940ad7027211b3b4d45b094ab11b7
muxanz <muxanzz@gmail.com>
2019-05-17 04:25:48 +0000

    corrects betty

--- _nop.c
+++ _nop.c
@@ -3,11 +3,15 @@
 #define UNUSED(x) (void)(x)
 
 /**
+ * _nop - doesn’t do anything.
  *
+ *@stack: pointer to the memory stack
  *
+ *@line_number: actual line number of the current opened file
  *
- *
-*/
+ * Return: void.
+ */
+
 
 void _nop(stack_t **stack, unsigned int line_number)
 {
